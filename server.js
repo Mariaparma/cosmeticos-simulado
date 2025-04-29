34f6f6111c5204e1cf452e75a9e4e8115c314290
maria parma <mariaparma.tds1.senai@gmail.com>
2025-04-29 11:32:02 -0300

    fix: update in sever

--- server.js
+++ server.js
@@ -4,28 +4,28 @@ const marcaRoutes = require('./src/routes/marcaRoutes.js');
 const cosmeticosRoutes = require('./src/routes/comesticosRoutes.js');
 const setupSwagger = require('./src/config/swagger.js');
 
-// Middleware para interpretar JSON
+
 app.use(express.json());
 
-// Configuração do Swagger
+
 setupSwagger(app);
 
-// Rotas
+
 app.use('/api/marcas', marcaRoutes); 
 app.use('/api/cosmeticos', cosmeticosRoutes);
 
-// Middleware para tratar rotas não encontradas
+
 app.use((req, res, next) => {
     res.status(404).json({ message: "Rota não encontrada." });
 });
 
-// Middleware para tratar erros gerais
+
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: "Erro interno do servidor." });
 });
 
-// Porta do servidor
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
